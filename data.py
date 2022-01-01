acb9f567aefc05d1cefdd6ca1ad4d4065b845046
Fedor Grab <fvgrab@gmail.com>
2022-01-01 15:50:34 -0500

    Setup GithubActions Pipe

---
+++ data.py
@@ -0,0 +1,77 @@
+import torch
+import pytorch_lightning as pl
+import torchvision
+from torchvision import transforms
+
+
+CROP_SIZE = 224
+TRAIN_BATCH_SIZE = 256
+TEST_BATCH_SIZE = 128
+BATCH_SIZE = 512
+NUM_EPOCHS = 40
+
+#train_data_dir = "./data_split/train"
+train_data_dir = "./data/train"
+val_data_dir = "./data_split/val"
+test_data_dir = "./data_split/val"
+
+
+class GarbageDataModule(pl.LightningDataModule):
+    def __init__(self, batch_size, data_dir: str = "./data_all"):
+        super().__init__()
+        self.data_dir = data_dir
+        self.batch_size = batch_size
+
+        # Augmentation policy for training set
+        self.augmentation = transforms.Compose(
+            [
+                transforms.ToTensor(),
+                transforms.Resize((CROP_SIZE, CROP_SIZE)),
+                transforms.RandomCrop((CROP_SIZE, CROP_SIZE)),
+                transforms.RandomAffine(degrees=5, scale=(1., 1.6), ),
+                transforms.RandomAutocontrast(),
+                #transforms.RandomApply([transforms.ColorJitter(0.1, 0.10, 0.1, 0.1)]),
+                transforms.Normalize(
+                    mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+                ),
+            ]
+        )
+        # Preprocessing steps applied to validation and test set.
+        self.transform = transforms.Compose(
+            [
+                transforms.ToTensor(),
+                transforms.Resize(size=(CROP_SIZE, CROP_SIZE)),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+            ]
+        )
+
+        self.num_classes = 8
+
+    def setup(self, stage=None):
+        # build dataset
+        self.train_dataset = torchvision.datasets.ImageFolder(train_data_dir)
+#         self.test_dataset = torchvision.datasets.ImageFolder(test_data_dir)
+        self.test_dataset = torchvision.datasets.ImageFolder(train_data_dir)
+
+        self.train_dataset.transform = self.augmentation
+        self.test_dataset.transform = self.transform
+
+    def train_dataloader(self):
+        return torch.utils.data.DataLoader(
+            self.train_dataset,
+            batch_size=self.batch_size,
+            shuffle=True,
+            num_workers=4,
+            pin_memory=True,
+        )
+
+
+    def test_dataloader(self):
+        return torch.utils.data.DataLoader(
+            self.test_dataset,
+            batch_size=self.batch_size,
+            shuffle=True,
+            num_workers=4,
+            pin_memory=True,
+        )
+
---
+++ train.py
@@ -0,0 +1,45 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import torch
+import pytorch_lightning as pl
+from neptune.new.integrations.pytorch_lightning import NeptuneLogger
+import model
+import data
+
+
+if __name__ == "__main__":
+    classifier = model.GarbageClassifier()
+    dm = data.GarbageDataModule(batch_size=8)
+    dm.setup()
+
+    val_samples = next(iter(dm.train_dataloader()))
+    val_imgs, val_labels = val_samples[0], val_samples[1]
+    val_imgs.shape, val_labels.shape
+
+    fig, axs = plt.subplots(nrows=1, ncols=5, figsize=(10,2))
+    imgs_pl = val_imgs[:5].numpy().transpose((0, 2, 3, 1))
+
+    for i, (img, label) in enumerate(zip(imgs_pl, val_labels)):
+        axs[i].imshow(img)
+        axs[i].set_xticks([])
+        axs[i].set_yticks([])
+        axs[i].set_title(f"Label = {dm.train_dataloader().dataset.classes[label.item()]}")
+    
+    neptune_logger = NeptuneLogger(
+        api_key="eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiJkNjNhOTY3ZS1mOGU2LTQ2ZGItYTFmOS01MGY4ZDdiNGU1YTcifQ==",
+        project="fedorgrab/garbage",
+        name="Resnet Pytorch Lightning My Data",
+    )
+    neptune_logger.experiment["dataset_sample"].upload(fig)
+
+    trainer = pl.Trainer(
+        max_epochs=1,
+        progress_bar_refresh_rate=20,
+        gpus=0,
+        logger=neptune_logger,
+    )
+
+    trainer.fit(classifier, dm)
+
+    torch.save(classifier.state_dict(), "new_model.pt")
+    neptune_logger.experiment["model"].upload("new_model.pt")
